10456598f44be27d9f293a4c6a7210931ff2858c
Alex Bertram <alex@bedatadriven.com>
2015-04-15 23:37:12 +0200

    AI-822 Adding changes missing from previous commit

--- server/src/main/java/org/activityinfo/ui/client/component/form/field/suggest/InstanceSuggestOracle.java
+++ server/src/main/java/org/activityinfo/ui/client/component/form/field/suggest/InstanceSuggestOracle.java
@@ -3,7 +3,7 @@ package org.activityinfo.ui.client.component.form.field.suggest;
 import com.google.gwt.user.client.ui.SuggestOracle;
 import org.activityinfo.model.form.FormInstance;
 import org.activityinfo.model.form.FormInstanceLabeler;
-import org.activityinfo.core.shared.importing.match.names.LatinPlaceNameScorer;
+import org.activityinfo.io.match.names.LatinPlaceNameScorer;
 
 import java.util.ArrayList;
 import java.util.List;
