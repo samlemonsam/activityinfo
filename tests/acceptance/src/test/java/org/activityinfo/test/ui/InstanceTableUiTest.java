a162714675ecf0a406242ef4ce2386dcd5a92bee
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-06-22 16:12:35 +0300

    AI-1103 : corrected acceptance test (with pre-fetching we are one round ahead)

--- tests/acceptance/src/test/java/org/activityinfo/test/ui/InstanceTableUiTest.java
+++ tests/acceptance/src/test/java/org/activityinfo/test/ui/InstanceTableUiTest.java
@@ -157,7 +157,7 @@ public class InstanceTableUiTest {
     private static void assertRowCount(int rowCount, int iteration) {
         assertTrue("rowCount: " + rowCount + ", expected to be more then: " + LOAD_COUNT * iteration +
                         ", end less/equals then: " + (LOAD_COUNT * (iteration + 1)),
-                rowCount > LOAD_COUNT * iteration && rowCount <= (LOAD_COUNT * (iteration + 1)));
+                rowCount > LOAD_COUNT * iteration /*&& rowCount <= (LOAD_COUNT * (iteration + 1))*/);
     }
 
 }
