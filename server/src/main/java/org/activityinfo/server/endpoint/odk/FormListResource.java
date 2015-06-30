607064e445b8f794f5a5270c186df64aed7d16f0
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-06-30 10:51:01 +0300

    fixed bad merge

--- server/src/main/java/org/activityinfo/server/endpoint/odk/FormListResource.java
+++ server/src/main/java/org/activityinfo/server/endpoint/odk/FormListResource.java
@@ -1,17 +1,16 @@
 package org.activityinfo.server.endpoint.odk;
 
-import com.google.common.collect.Maps;
-import com.sun.jersey.api.view.Viewable;
+import com.google.inject.Inject;
 import org.activityinfo.legacy.shared.command.GetSchema;
 import org.activityinfo.legacy.shared.model.ActivityDTO;
 import org.activityinfo.legacy.shared.model.SchemaDTO;
 import org.activityinfo.legacy.shared.model.UserDatabaseDTO;
-import org.activityinfo.server.util.monitoring.Timed;
-legacy.shared.model.ActivityDTO;
-import org.activityinfo.legacy.shared.model.SchemaDTO;
-import org.activityinfo.legacy.shared.model.UserDatabaseDTO;
-import org.activityinfo.server.util.monitoring.Timed;
+import org.activityinfo.model.auth.AuthenticatedUser;
+import org.activityinfo.server.command.DispatcherSync;
+import org.activityinfo.io.xform.formList.XFormList;
+import org.activityinfo.io.xform.formList.XFormListItem;
 
+import javax.inject.Provider;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,74 +18,66 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.logging.Logger;
 
 @Path("/formList")
-public class FormListResource extends ODKResource {
+public class FormListResource {
+    private static final Logger LOGGER = Logger.getLogger(FormListResource.class.getName());
 
+
+
+    private Provider<AuthenticatedUser> authProvider;
+    private DispatcherSync dispatcher;
+
+    @Inject
+    public FormListResource(OdkAuthProvider authProvider, DispatcherSync dispatcher) {
+        this.authProvider = authProvider;
+        this.dispatcher = dispatcher;
+    }
 
     @GET
-    @Timed(name = "odk", kind = "formList")
     @Produces(MediaType.TEXT_XML)
-    public Response formList(@Context UriInfo info) throws Exception {
-        if (enforceAuthorization()) {
-            return askAuthentication();
-        }
-        LOGGER.finer("ODK formlist requested by " + getUser().getEmail() + " (" + getUser().getId() + ")");
+    public Response formList(@Context UriInfo uri) throws Exception {
+        AuthenticatedUser user = authProvider.get();
 
-        List<ActivityDTO> activities = new ArrayList<>();
+        LOGGER.finer("ODK form list requested by " + user.getEmail() + " (" + user.getId() + ")");
+
         SchemaDTO schema = dispatcher.execute(new GetSchema());
 
-        for (UserDatabaseDTO database : schema.getDatabases()) {
-            if(canBeSubmittedViaOdk(database)) {
-                for (ActivityDTO activity : database.getActivities()) {
-                    if (canBeSubmittedViaOdk(activity)) {
-                        activities.add(activity);
+        XFormList formList = new XFormList();
+        for (UserDatabaseDTO db : schema.getDatabases()) {
+            if (db.isEditAllowed()) {
+                for (ActivityDTO activity : db.getActivities()) {
+                    XFormListItem form = new XFormListItem();
+                    form.setName(db.getName() + " / " + activity.getName());
+                    form.setFormId("activityinfo.org:" + activity.getId());
+                    form.setVersion(getVersion());
+
+                    form.setDownloadUrl(uri.getBaseUriBuilder()
+                            .path(FormResource.class)
+                            .path(Integer.toString(activity.getId()))
+                            .path("xform")
+                            .build());
+
+                    // skip itemset for LCCA form: earlier versions triggered
+                    // a bug in ODK and have corrupted the sqlite database on those
+                    // devices.
+                    if(activity.getId() != 11218) {
+                        form.setManifestUrl(uri.getBaseUriBuilder()
+                                .path(FormResource.class)
+                                .path(Integer.toString(activity.getId()))
+                                .path("manifest")
+                                .build());
                     }
+
+                    formList.getItems().add(form);
                 }
             }
         }
-
-        Map<String, Object> map = Maps.newHashMap();
-        map.put("activities", activities);
-        map.put("host", info.getBaseUri().toString());
-
-        return Response.ok(new Viewable("/odk/formList.ftl", map)).build();
+        return OpenRosaResponse.build(formList);
     }
 
-    private boolean canBeSubmittedViaOdk(UserDatabaseDTO database) {
-        // Edit permissions required
-        if(!database.isEditAllowed()) {
-            return false;
-        }
-        // Forms cannot be submitted without a partner 
-        if(database.getPartners().isEmpty()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Returns true if the activity can be entered as an ODK form.
-     */
-    private boolean canBeSubmittedViaOdk(ActivityDTO activity) {
-
-        // only "once" activities can be submitted via odk because
-        // we don't have a good way of allowing users to choose 
-        // the site or edit existing monthly reports
-        if (activity.getReportingFrequency() == ActivityDTO.REPORT_MONTHLY) {
-            return false;
-        }
-
-        // Activities bound to admin levels cannot be submitted via ODK
-        // because of problems with cascading lists
-        if (activity.getLocationType().isAdminLevel()) {
-            return false;
-        }
-
-        return true;
+    private String getVersion() {
+        return Long.toString(System.currentTimeMillis() / 1000);
     }
 }
