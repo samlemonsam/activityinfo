samlemonsam/activityinfo

commit 972c152152005f773d2dcab9f04cb3dfe1c0259a

AI-1158 : added loading mask and retry functionality to report views.

server/src/main/java/org/activityinfo/ui/client/component/report/view/ChartOFCView.java
@@ -43,6 +43,7 @@ import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.MarginData;
 import com.google.common.collect.Lists;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Element;
 import org.activityinfo.i18n.shared.I18N;
@@ -115,6 +116,18 @@ public class ChartOFCView extends ContentPanel implements ChartView {
         }
     }
 
+    @Override
+    public void loading() {
+        el().mask();
+    }
+
+    @Override
+    public void onFailure(Throwable caught, ClickHandler retryCallback) {
+        ReportViewRetrier.onFailure(this, caught, retryCallback);
+        chart = null;
+    }
+
+
     /**
      * Updates the view to the given PivotChartContent
      *
@@ -122,6 +135,7 @@ public class ChartOFCView extends ContentPanel implements ChartView {
      */
     @Override
     public void show(PivotChartReportElement element) {
+        el().unmask();
 
         PivotChartContent content = element.getContent();
         PivotTableData table = element.getContent().getData();

server/src/main/java/org/activityinfo/ui/client/component/report/view/MapReportView.java
@@ -28,6 +28,7 @@ import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.google.gwt.event.dom.client.ClickHandler;
 import org.activityinfo.legacy.shared.reports.model.MapReportElement;
 import org.activityinfo.ui.client.component.report.editor.map.LeafletMap;
 import org.activityinfo.ui.client.component.report.editor.map.symbols.LeafletReportOverlays;
@@ -49,7 +50,20 @@ public class MapReportView extends ContentPanel implements ReportView<MapReportE
     }
 
     @Override
+    public void loading() {
+        el().mask();
+    }
+
+    @Override
+    public void onFailure(Throwable caught, ClickHandler retryCallback) {
+        ReportViewRetrier.onFailure(this, caught, retryCallback);
+        map = null;
+    }
+
+
+    @Override
     public void show(MapReportElement element) {
+        el().unmask();
         this.element = element;
         addContent();
     }

server/src/main/java/org/activityinfo/ui/client/component/report/view/PivotGridPanel.java
@@ -34,6 +34,7 @@ import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.grid.*;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.google.gwt.event.dom.client.ClickHandler;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.legacy.client.Dispatcher;
 import org.activityinfo.legacy.client.type.IndicatorNumberFormat;
@@ -87,7 +88,19 @@ public class PivotGridPanel extends ContentPanel implements ReportView<PivotRepo
     }
 
     @Override
+    public void loading() {
+        el().mask();
+    }
+
+    @Override
+    public void onFailure(Throwable caught, ClickHandler retryCallback) {
+        ReportViewRetrier.onFailure(this, caught, retryCallback);
+    }
+
+    @Override
     public void show(final PivotReportElement element) {
+        el().unmask();
+
         if (grid != null) {
             removeAll();
         }

server/src/main/java/org/activityinfo/ui/client/component/report/view/ReportView.java
@@ -23,12 +23,16 @@ package org.activityinfo.ui.client.component.report.view;
  */
 
 import com.extjs.gxt.ui.client.widget.Component;
+import com.google.gwt.event.dom.client.ClickHandler;
 import org.activityinfo.legacy.shared.reports.model.ReportElement;
 
 public interface ReportView<T extends ReportElement> {
+
+    void loading();
 
     void show(T element);
 
     Component asComponent();
 
+    void onFailure(Throwable caught, ClickHandler retryCallback);
 }

server/src/main/java/org/activityinfo/ui/client/component/report/view/ReportViewBinder.java
@@ -23,6 +23,8 @@ package org.activityinfo.ui.client.component.report.view;
  */
 
 import com.extjs.gxt.ui.client.widget.Component;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import org.activityinfo.legacy.client.Dispatcher;
@@ -104,11 +106,18 @@ public class ReportViewBinder<C extends Content, R extends ReportElement<C>> imp
             
             latestRequest = request;
 
+            view.loading();
             dispatcher.execute(request, new AsyncCallback<C>() {
 
                 @Override
                 public void onFailure(Throwable caught) {
                     Log.error(caught.getMessage(), caught);
+                    view.onFailure(caught, new ClickHandler() {
+                        @Override
+                        public void onClick(ClickEvent event) {
+                            load();
+                        }
+                    });
                 }
 
                 @Override

server/src/main/java/org/activityinfo/ui/client/component/report/view/ReportViewRetrier.java
@@ -1,0 +1,70 @@
+package org.activityinfo.ui.client.component.report.view;
+/*
+ * #%L
+ * ActivityInfo Server
+ * %%
+ * Copyright (C) 2009 - 2013 UNICEF
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.extjs.gxt.ui.client.Style;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.util.Margins;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.Text;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.layout.RowData;
+import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.google.gwt.event.dom.client.ClickHandler;
+import org.activityinfo.i18n.shared.I18N;
+
+/**
+ * @author yuriyz on 07/14/2015.
+ */
+public class ReportViewRetrier {
+
+    public static void onFailure(LayoutContainer container, Throwable caught, ClickHandler retryCallback) {
+        container.el().unmask();
+        container.removeAll();
+        container.add(createRetryPanel(caught, retryCallback));
+        container.layout();
+    }
+
+    private static ContentPanel createRetryPanel(final Throwable caught, final ClickHandler retryCallback) {
+        Button retryButton = new Button(I18N.CONSTANTS.retry());
+        retryButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                if (retryCallback != null) {
+                    retryCallback.onClick(null);
+                }
+            }
+        });
+
+        Text label = new Text(I18N.CONSTANTS.failedToLoadEntries() + ". Reason : " + caught.getMessage());
+
+        ContentPanel panel = new ContentPanel();
+        panel.setHeaderVisible(false);
+        panel.setLayout(new RowLayout(Style.Orientation.VERTICAL));
+        panel.add(label, new RowData(1, -1, new Margins(4)));
+        panel.add(retryButton, new RowData(-1, -1, new Margins(4)));
+
+        return panel;
+    }
+}

commit 7421ac9173ce1d37780d739f2d39243fae453119

AI-1172 Fixed regressions to reports introduced by 972c152152005f773d2dcab9f04cb3dfe1c0259a

server/src/main/java/org/activityinfo/ui/client/component/report/view/ChartOFCView.java
@@ -118,7 +118,9 @@ public class ChartOFCView extends ContentPanel implements ChartView {
 
     @Override
     public void loading() {
-        el().mask();
+        if(isRendered()) {
+            el().mask();
+        }
     }
 
     @Override
@@ -135,7 +137,9 @@ public class ChartOFCView extends ContentPanel implements ChartView {
      */
     @Override
     public void show(PivotChartReportElement element) {
-        el().unmask();
+        if(isRendered()) {
+            el().unmask();
+        }
 
         PivotChartContent content = element.getContent();
         PivotTableData table = element.getContent().getData();

server/src/main/java/org/activityinfo/ui/client/component/report/view/MapReportView.java
@@ -51,7 +51,9 @@ public class MapReportView extends ContentPanel implements ReportView<MapReportE
 
     @Override
     public void loading() {
-        el().mask();
+        if(isRendered()) {
+            el().mask();
+        }
     }
 
     @Override
@@ -63,7 +65,9 @@ public class MapReportView extends ContentPanel implements ReportView<MapReportE
 
     @Override
     public void show(MapReportElement element) {
-        el().unmask();
+        if(isRendered()) {
+            el().unmask();
+        }
         this.element = element;
         addContent();
     }

server/src/main/java/org/activityinfo/ui/client/component/report/view/PivotGridPanel.java
@@ -89,7 +89,9 @@ public class PivotGridPanel extends ContentPanel implements ReportView<PivotRepo
 
     @Override
     public void loading() {
-        el().mask();
+        if(isRendered()) {
+            el().mask();
+        }
     }
 
     @Override
@@ -99,7 +101,9 @@ public class PivotGridPanel extends ContentPanel implements ReportView<PivotRepo
 
     @Override
     public void show(final PivotReportElement element) {
-        el().unmask();
+        if(isRendered()) {
+            el().unmask();
+        }
 
         if (grid != null) {
             removeAll();