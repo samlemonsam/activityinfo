29f0f0847f795a716224b87d266f8cafc43676f2
Alex Bertram <alex@bedatadriven.com>
2015-08-27 15:44:38 +0200

    AI-1206 Fixed assembly versioning

--- clients/dotnet/Client/Properties/AssemblyInfo.cs
+++ clients/dotnet/Client/Properties/AssemblyInfo.cs
@@ -10,14 +10,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany ("BeDataDriven")]
 [assembly: AssemblyProduct ("ActivityInfo Client")]
 [assembly: AssemblyCopyright ("BeDataDriven BV")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyCulture ("en_us")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion ("2.9.*")]
+// The assembly version will be updated by the assemblyInfoPatcher gradle task
+[assembly: AssemblyVersion("2.9.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
