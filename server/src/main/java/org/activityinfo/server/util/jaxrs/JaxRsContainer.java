af3c53558f6998122c955ec727f6896d9978aff5
Alex Bertram <alex@bedatadriven.com>
2015-09-04 11:24:49 +0200

    AI-1215 Ensured that all responses include a charset parameter to the Content-Type header

---
+++ server/src/main/java/org/activityinfo/server/util/jaxrs/JaxRsContainer.java
@@ -0,0 +1,40 @@
+package org.activityinfo.server.util.jaxrs;
+
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+import com.sun.jersey.api.core.DefaultResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import com.sun.jersey.spi.container.servlet.WebConfig;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Subclass of GuiceContainer that registers ContainerResponseFilters. 
+ */
+@Singleton
+public class JaxRsContainer extends GuiceContainer {
+    /**
+     * Creates a new Injector.
+     *
+     * @param injector the Guice injector
+     */
+    @Inject
+    public JaxRsContainer(Injector injector) {
+        super(injector);
+    }
+
+    @Override
+    protected ResourceConfig getDefaultResourceConfig(Map<String, Object> props, WebConfig webConfig) throws ServletException {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, CharsetResponseFilter.class.getName());
+        
+        DefaultResourceConfig config = new DefaultResourceConfig();
+        config.setPropertiesAndFeatures(parameters);
+        
+        return config;
+    }
+}
