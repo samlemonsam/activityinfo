c242f98fd3c3a8b4b43d1fa9500982df8378dac8
Alex Bertram <alex@bedatadriven.com>
2015-10-09 09:21:55 +0200

    AI-822 Updated FieldScoreMatrix to use LatinPlaceNameMatcher for scoring
    This requires a lot more time to compute the matrix but yields mutch better
    results when the majority of matches between fields are partial.

--- geoadmin/src/main/java/org/activityinfo/geoadmin/merge2/view/match/FieldScoreMatrix.java
+++ geoadmin/src/main/java/org/activityinfo/geoadmin/merge2/view/match/FieldScoreMatrix.java
@@ -1,12 +1,13 @@
 package org.activityinfo.geoadmin.merge2.view.match;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import org.activityinfo.geoadmin.match.ScoreMatrix;
 import org.activityinfo.geoadmin.merge2.view.profile.FieldProfile;
+import org.activityinfo.io.match.names.LatinPlaceNameScorer;
+import org.activityinfo.model.query.ColumnView;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * Computes the "distance" between the <em>contents</em> of two fields
@@ -16,9 +17,17 @@ public class FieldScoreMatrix extends ScoreMatrix {
     private List<FieldProfile> sourceColumns;
     private List<FieldProfile> targetColumns;
 
+    private double[][] scores;
+    
     public FieldScoreMatrix(Iterable<FieldProfile> sourceColumns, Iterable<FieldProfile> targetColumns) {
         this.sourceColumns = Lists.newArrayList(sourceColumns);
         this.targetColumns = Lists.newArrayList(targetColumns);
+        this.scores = new double[this.sourceColumns.size()][this.targetColumns.size()];
+        for (int i = 0; i < this.sourceColumns.size(); i++) {
+            for (int j = 0; j < this.targetColumns.size(); j++) {
+                scores[i][j] = calculateScore(i, j);
+            }
+        }
     }
 
     @Override
@@ -39,6 +48,10 @@ public class FieldScoreMatrix extends ScoreMatrix {
 
     @Override
     public double score(int i, int j, int d) {
+        return scores[i][j];
+    }
+
+    private double calculateScore(int i, int j) {
         FieldProfile x = sourceColumns.get(i);
         FieldProfile y = targetColumns.get(j);
 
@@ -58,20 +71,60 @@ public class FieldScoreMatrix extends ScoreMatrix {
         return 1.0;
     }
 
-    private double scoreTextColumnMatch(FieldProfile x, FieldProfile y) {
-        Set<String> sourceValues = x.uniqueValues();
-        Set<String> targetValues = y.uniqueValues();
-
-        // Alternative: Relative information
-        // I(s; p) = sum(i = 1 to n)  s[i] * log(s[i]/p[i])
-
-        double intersection = Sets.intersection(targetValues, sourceValues).size();
-        if (intersection > 0) {
-            double union = Sets.union(targetValues, sourceValues).size();
-            return intersection / union;
-
+    private double scoreTextColumnMatch(FieldProfile sourceProfile, FieldProfile targetProfile) {
+        ColumnView source = sourceProfile.getView();
+        ColumnView target = targetProfile.getView();
+        
+        if(source.numRows() <= target.numRows()) {
+            return scoreTextColumnMatch(source, target);
         } else {
-            return 0d;
+            return scoreTextColumnMatch(target, source);
         }
+    }
+    
+    private double scoreTextColumnMatch(ColumnView x, ColumnView y) {
+        assert x.numRows() <= y.numRows();
+        
+        
+        // For each of the rows in the smaller of the source / target sets,
+        // find the score of the best match in the opposing set.
+        // 
+        // the score of the column pairs is then the mean of the row maxes
+
+        
+        double sumRowMaxes = 0;
+        int countOfRows = 0;
+        
+        for (int i = 0; i < x.numRows(); i++) {
+            String rowName = x.getString(i);
+            if(!Strings.isNullOrEmpty(rowName)) {
+                double maxScore = findScoreOfBestMatch(rowName, y);
+                
+                sumRowMaxes += maxScore;
+                countOfRows++;
+            }
+        }
+        
+        if(countOfRows == 0) {
+            return 0;
+        } else {
+            return sumRowMaxes / (double)countOfRows;
+        }
+    }
+
+    private double findScoreOfBestMatch(String rowName, ColumnView y) {
+        LatinPlaceNameScorer scorer = new LatinPlaceNameScorer();
+        
+        double maxScore = 0;
+        for (int j = 0; j < y.numRows(); j++) {
+            String colName = y.getString(j);
+            if(!Strings.isNullOrEmpty(colName)) {
+                double score = scorer.score(rowName, colName);
+                if(score > maxScore) {
+                    maxScore = score;
+                }
+            }
+        }
+        return maxScore;
     }
 }
