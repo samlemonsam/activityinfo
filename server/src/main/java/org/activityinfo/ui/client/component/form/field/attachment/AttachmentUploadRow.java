c74d7be183452abb306ea03d2544aa6d41daf677
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-12-03 09:46:06 +0200

    AI-618 , AI-665 : fixed attachments downloading

--- server/src/main/java/org/activityinfo/ui/client/component/form/field/attachment/AttachmentUploadRow.java
+++ server/src/main/java/org/activityinfo/ui/client/component/form/field/attachment/AttachmentUploadRow.java
@@ -125,7 +125,10 @@ public class AttachmentUploadRow extends Composite {
             requestBuilder.sendRequest(null, new RequestCallback() {
                 @Override
                 public void onResponseReceived(Request request, Response response) {
-                    servingUrl = response.getText();
+                    String url = response.getText();
+                    if (url.startsWith("http")) {
+                        servingUrl = url;
+                    }
                     setState();
                 }
 
