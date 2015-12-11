0130517a4d22c22c83c6521666db31fa0bef9a2d
Alex Bertram <alex@bedatadriven.com>
2015-12-11 11:31:23 +0100

    AI-944 Updated AdminDimBinding to use @.id expression

--- server/src/main/java/org/activityinfo/server/command/handler/pivot/AdminDimBinding.java
+++ server/src/main/java/org/activityinfo/server/command/handler/pivot/AdminDimBinding.java
@@ -3,16 +3,13 @@ package org.activityinfo.server.command.handler.pivot;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
-import com.google.common.collect.Iterables;
 import org.activityinfo.legacy.shared.reports.content.DimensionCategory;
 import org.activityinfo.legacy.shared.reports.content.EntityCategory;
-import org.activityinfo.legacy.shared.reports.content.SimpleCategory;
 import org.activityinfo.legacy.shared.reports.model.AdminDimension;
 import org.activityinfo.legacy.shared.reports.model.Dimension;
 import org.activityinfo.model.form.FormClass;
 import org.activityinfo.model.formTree.FieldPath;
 import org.activityinfo.model.formTree.FormTree;
-import org.activityinfo.model.legacy.CuidAdapter;
 import org.activityinfo.model.query.ColumnModel;
 import org.activityinfo.model.query.ColumnSet;
 import org.activityinfo.model.query.ColumnView;
@@ -49,16 +46,15 @@ public class AdminDimBinding extends DimBinding {
         Optional<FormClass> adminClass = formTree.getFormClassIfPresent(levelClassId);
         
         if(adminClass.isPresent()) {
-//            FieldPath path = new FieldPath(levelClassId);
-//            ColumnModel id = new ColumnModel();
-//            id.setExpression(path);
-//            id.setId(idColumn);
+            ColumnModel id = new ColumnModel();
+            id.setExpression(levelClassId + "." + ColumnModel.ID_SYMBOL);
+            id.setId(idColumn);
             
             ColumnModel label = new ColumnModel();
             label.setExpression(new FieldPath(levelClassId, field(levelClassId, NAME_FIELD)));
             label.setId(labelColumn);
             
-            return Arrays.asList(label);
+            return Arrays.asList(id, label);
         } else {
             return Collections.emptyList();
         }
@@ -82,19 +78,16 @@ public class AdminDimBinding extends DimBinding {
     public DimensionCategory[] extractCategories(ActivityMetadata activity, FormTree formTree, ColumnSet columnSet) {
         DimensionCategory[] c = new DimensionCategory[columnSet.getNumRows()];
         if(columnSet.getColumns().containsKey(labelColumn)) {
-          //  ColumnView idView = columnSet.getColumnView(idColumn);
+            ColumnView idView = columnSet.getColumnView(idColumn);
             ColumnView labelView = columnSet.getColumnView(labelColumn);
             
             for (int i = 0; i < columnSet.getNumRows(); i++) {
-              //  String id = idView.getString(i);
-               // if(id != null) {
-                //    int entityId = getLegacyIdFromCuid(id);
+                String id = idView.getString(i);
+                if(id != null) {
+                    int entityId = getLegacyIdFromCuid(id);
                     String label = labelView.getString(i);
-                    if(label != null) {
-                        c[i] = new SimpleCategory(label);
-                    }
-                   // c[i] = new EntityCategory(entityId, label);
-                //}
+                    c[i] = new EntityCategory(entityId, label);
+                }
             }
         }
         return c;
