feab8595f5b51874d2e8d2c28656882f29575ee5
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-12-31 15:44:37 +0200

    AI-1308 : made enum widget clear button behavior compatible with image widget.

--- server/src/main/java/org/activityinfo/ui/client/component/form/field/EnumListFieldWidget.java
+++ server/src/main/java/org/activityinfo/ui/client/component/form/field/EnumListFieldWidget.java
@@ -84,6 +84,7 @@ public class EnumListFieldWidget implements FormFieldWidget<EnumValue> {
     private final List<CheckBox> controls;
     private final FieldWidgetMode fieldWidgetMode;
     private final ValueUpdater<EnumValue> valueUpdater;
+    private final Button clearButton = createClearButton();
 
     public EnumListFieldWidget(EnumType enumType, final ValueUpdater<EnumValue> valueUpdater, FieldWidgetMode fieldWidgetMode) {
         this.enumType = enumType;
@@ -106,8 +107,9 @@ public class EnumListFieldWidget implements FormFieldWidget<EnumValue> {
         }
 
         panel = new FlowPanel();
+        panel.addStyleName("hide-button-on-over");
         if (enumType.getCardinality() == Cardinality.SINGLE) {
-            panel.add(createClearButton());
+            panel.add(clearButton);
         }
         panel.add(boxPanel);
         if (this.fieldWidgetMode == FieldWidgetMode.DESIGN) {
@@ -143,11 +145,18 @@ public class EnumListFieldWidget implements FormFieldWidget<EnumValue> {
                 }
             }
         });
+        clearButton.setVisible(false);
         return clearButton;
     }
 
+    private void setClearButtonState(EnumValue enumValue) {
+        clearButton.setVisible(enumValue != null && !enumValue.getResourceIds().isEmpty());
+    }
+
     public void fireValueChanged() {
-        valueUpdater.update(updatedValue());
+        EnumValue value = updatedValue();
+        valueUpdater.update(value);
+        setClearButtonState(value);
     }
 
     private void onClick(MouseUpEvent event) {
@@ -251,6 +260,7 @@ public class EnumListFieldWidget implements FormFieldWidget<EnumValue> {
         for (CheckBox entry : controls) {
             entry.setValue(containsIgnoreCase(value.getResourceIds(), entry.getFormValue()));
         }
+        setClearButtonState(value);
         return Promise.done();
     }
 
