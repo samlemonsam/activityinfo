1408034913986aff797b758262d9c50e6d8c02d9
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-01-26 10:44:15 +0200

    ai-1189 : NPE fix

--- server/src/main/java/org/activityinfo/legacy/shared/impl/GetSitesHandler.java
+++ server/src/main/java/org/activityinfo/legacy/shared/impl/GetSitesHandler.java
@@ -796,6 +796,7 @@ public class GetSitesHandler implements CommandHandlerAsync<GetSites, SiteResult
                 .appendColumn("I.Expression", "expression")
                 .appendColumn("I.nameInExpression", "code")
                 .appendColumn("I.calculatedAutomatically", "calculatedAutomatically")
+                .appendColumn("I.typeJson", "typeJson")
                 .from(Tables.INDICATOR, "I")
                 .where("I.ActivityId")
                 .in(activityIds)
@@ -845,6 +846,7 @@ public class GetSitesHandler implements CommandHandlerAsync<GetSites, SiteResult
         indicator.setNameInExpression(rs.getString("code"));
         indicator.setCalculatedAutomatically(rs.getBoolean("calculatedAutomatically"));
         indicator.setUnits(rs.getString("units"));
+        indicator.setTypeJson(rs.getString("typeJson"));
         return indicator.asFormField();
     }
 
