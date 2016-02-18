1582af61fd30a05c0093744059fa9d0ccfc2b302
Alex Bertram <alex@bedatadriven.com>
2016-02-18 11:19:42 +0100

    AI-1078 Override RTL orientation for arabic locale
    The ArabicLTR gwt module forces the overal application layout to LTR
    when compiling for arabic locale.

--- server/src/main/java/org/activityinfo/ui/client/page/config/design/DesignView.java
+++ server/src/main/java/org/activityinfo/ui/client/page/config/design/DesignView.java
@@ -155,7 +155,7 @@ public class DesignView extends AbstractEditorTreeGridView<ModelData, DesignPres
                         return IconImageBundle.ICONS.form();
                     }
                 } else if (model instanceof Folder) {
-                    return AbstractImagePrototype.create(GXT.IMAGES.tree_folder_closed());
+                    return GXT.IMAGES.tree_folder_closed();
                 } else if (model instanceof AttributeGroupDTO) {
                     return IconImageBundle.ICONS.attributeGroup();
                 } else if (model instanceof AttributeDTO) {
