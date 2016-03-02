c2bf9984e8bbfd09a1f5515a8f8402105f1aa1ec
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-03-02 14:25:16 +0200

    AI-664 : removed redundant test

--- core/model/src/test/java/org/activityinfo/model/form/FormClassTest.java
+++ core/model/src/test/java/org/activityinfo/model/form/FormClassTest.java
@@ -1,16 +1,12 @@
 package org.activityinfo.model.form;
 
-import org.activityinfo.model.legacy.CuidAdapter;
 import org.activityinfo.model.resource.Resource;
 import org.activityinfo.model.resource.ResourceId;
-import org.activityinfo.model.type.ReferenceType;
 import org.activityinfo.model.type.primitive.TextType;
-import org.activityinfo.model.type.time.LocalDateType;
 import org.junit.Test;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class FormClassTest {
 
@@ -29,33 +25,6 @@ public class FormClassTest {
         FormClass reform = FormClass.fromResource(resource);
         assertThat(reform.getFields(), hasSize(1));
 
-    }
-
-    @Test
-    public void formFieldsOrderTest() {
-        FormClass formClass = new FormClass(ResourceId.generateId());
-        formClass.setOwnerId(ResourceId.ROOT_ID);
-        formClass.setLabel("Form");
-
-        FormField projectField = new FormField(CuidAdapter.field(formClass.getId(), CuidAdapter.PROJECT_FIELD))
-                .setLabel("Project field")
-                .setType(ReferenceType.single(CuidAdapter.projectFormClass(1)));
-        formClass.addElement(projectField);
-
-        FormField startDateField = new FormField(CuidAdapter.field(formClass.getId(), CuidAdapter.START_DATE_FIELD))
-                .setLabel("Start date")
-                .setType(LocalDateType.INSTANCE)
-                .setRequired(true);
-        formClass.addElement(startDateField);
-
-        FormField notBuiltIn = formClass.addField(ResourceId.generateId())
-                .setLabel("Built-in");
-
-        formClass.reorderFormFields();
-
-        assertTrue(formClass.getFields().indexOf(notBuiltIn) == 0);
-        assertTrue(formClass.getFields().indexOf(projectField) == 1);
-        assertTrue(formClass.getFields().indexOf(startDateField) == 2);
     }
 
 }
