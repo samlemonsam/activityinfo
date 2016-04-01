39158cbee3e13a5f8c9a8cb9e0f61c68bea349be
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-04-01 14:34:30 +0300

    AI-733 : commented out ability to specify text style

--- core/model/src/main/java/org/activityinfo/model/type/MetadataType.java
+++ core/model/src/main/java/org/activityinfo/model/type/MetadataType.java
@@ -21,20 +21,15 @@ package org.activityinfo.model.type;
  * #L%
  */
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.model.form.FormClass;
-import org.activityinfo.model.form.FormField;
 import org.activityinfo.model.resource.Record;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.model.resource.ResourceIdPrefixType;
-import org.activityinfo.model.type.enumerated.EnumItem;
-import org.activityinfo.model.type.enumerated.EnumType;
 
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * It's not really type but convenient way to keep metadata of formclass as formfield (typically invisible).
@@ -122,18 +117,18 @@ public class MetadataType implements ParametrizedFieldType {
         public FormClass getParameterFormClass() {
             FormClass formClass = new FormClass(ResourceIdPrefixType.TYPE.id("metadata"));
 
-            List<EnumItem> items = Lists.newArrayList();
-            for (TextStyle style : TextStyle.values()) {
-                EnumItem item = new EnumItem(style.getResourceId(), style.getLabel());
-                items.add(item);
-            }
-
-            EnumType enumType = new EnumType(Cardinality.SINGLE, items);
-            enumType.getDefaultValues().add(new EnumItem(TextStyle.PLAIN.getResourceId(), TextStyle.PLAIN.getLabel()));
-
-            formClass.addElement(new FormField(ResourceId.valueOf("text_style"))
-                    .setType(enumType)
-                    .setLabel(I18N.CONSTANTS.style()));
+//            List<EnumItem> items = Lists.newArrayList();
+//            for (TextStyle style : TextStyle.values()) {
+//                EnumItem item = new EnumItem(style.getResourceId(), style.getLabel());
+//                items.add(item);
+//            }
+//
+//            EnumType enumType = new EnumType(Cardinality.SINGLE, items);
+//            enumType.getDefaultValues().add(new EnumItem(TextStyle.PLAIN.getResourceId(), TextStyle.PLAIN.getLabel()));
+//
+//            formClass.addElement(new FormField(ResourceId.valueOf("text_style"))
+//                    .setType(enumType)
+//                    .setLabel(I18N.CONSTANTS.style()));
 
             return formClass;
         }
