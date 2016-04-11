82113fde686ac9da6bd0b241adf29f689d8d3995
Alex Bertram <alex@bedatadriven.com>
2016-04-11 11:11:38 +0200

    Fixes AISEC-3

--- server/src/main/java/org/activityinfo/ui/client/component/form/FormDialog.java
+++ server/src/main/java/org/activityinfo/ui/client/component/form/FormDialog.java
@@ -21,14 +21,16 @@ package org.activityinfo.ui.client.component.form;
  * #L%
  */
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import org.activityinfo.core.client.ResourceLocator;
-import org.activityinfo.model.form.FormInstance;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.legacy.shared.Log;
+import org.activityinfo.model.form.FormInstance;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.promise.Promise;
 import org.activityinfo.ui.client.component.form.field.FieldWidgetMode;
@@ -46,6 +48,13 @@ import javax.inject.Provider;
  */
 public class FormDialog {
 
+    interface Templates extends SafeHtmlTemplates {
+        @Template("<h3>{0}</h3><h4>{1}</h4>")
+        SafeHtml title(String title, String subtitle);
+    }
+    
+    private static final Templates TEMPLATES = GWT.create(Templates.class);
+    
     private FormDialogCallback callback;
 
     private final ResourceLocator resourceLocator;
@@ -83,9 +92,8 @@ public class FormDialog {
         dialog.setDialogTitle(text);
     }
 
-    public void setDialogTitle(String h3, String h4) {
-        String html = "<h3>" + h3 + "</h3><br/><h4>" + h4 + "</h4>" ;
-        dialog.setDialogTitle(SafeHtmlUtils.fromSafeConstant(html).asString());
+    public void setDialogTitle(String title, String subTitle) {
+        dialog.setDialogTitle(TEMPLATES.title(title, subTitle));
     }
 
     public void show(final FormInstance instance, FormDialogCallback callback) {
--- server/src/main/java/org/activityinfo/ui/client/page/entry/form/SiteDialogLauncher.java
+++ server/src/main/java/org/activityinfo/ui/client/page/entry/form/SiteDialogLauncher.java
@@ -122,9 +122,9 @@ public class SiteDialogLauncher {
     }
 
     public static void showModernFormDialog(String formName, FormInstance instance, final SiteDialogCallback callback, boolean isNew, ResourceLocator resourceLocator) {
-        String h2Title = isNew ? I18N.CONSTANTS.newSubmission() : I18N.CONSTANTS.editSubmission();
+        String subtitle = isNew ? I18N.CONSTANTS.newSubmission() : I18N.CONSTANTS.editSubmission();
         FormDialog dialog = new FormDialog(resourceLocator);
-        dialog.setDialogTitle(formName, h2Title);
+        dialog.setDialogTitle(formName, subtitle);
         dialog.show(instance, new FormDialogCallback() {
             @Override
             public void onPersisted(FormInstance instance) {
