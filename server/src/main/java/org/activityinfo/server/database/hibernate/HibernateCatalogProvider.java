3a9b318645bfddd884bcbe5f8ad4ff3abbe0e6d3
Alex Bertram <alex@bedatadriven.com>
2016-06-09 16:53:05 +0200

    Hooked the HrdCatalog into existing MySqlCatalog
    This makes it possible to query both legacy Mysql-based collections and
    collections stored in HRD with a consistent API:
    
    http://127.0.0.1:8080/resources/form/a68081/tree/pretty
    [NewestForm]
    ..Start Date
    ..End Date
    ..Comments
    ..Partner = [Partner]
    ..............Name
    ..............Full Name
    ..Project = [Project]
    ..............Name
    ..............Description
    ..Sub Form = [Sub Form]
    ...............Text
    ...............Quantity

--- server/src/main/java/org/activityinfo/server/database/hibernate/HibernateCatalogProvider.java
+++ server/src/main/java/org/activityinfo/server/database/hibernate/HibernateCatalogProvider.java
@@ -2,6 +2,8 @@ package org.activityinfo.server.database.hibernate;
 
 import com.google.inject.Provider;
 import org.activityinfo.service.store.CollectionCatalog;
+import org.activityinfo.service.store.CompositeCatalog;
+import org.activityinfo.store.hrd.HrdCatalog;
 import org.activityinfo.store.mysql.MySqlCatalogProvider;
 
 import javax.inject.Inject;
@@ -10,7 +12,9 @@ import javax.persistence.EntityManager;
 public class HibernateCatalogProvider implements Provider<CollectionCatalog> {
 
     private final MySqlCatalogProvider catalogProvider;
+    
     private final Provider<EntityManager> entityManager;
+    
 
     @Inject
     public HibernateCatalogProvider(MySqlCatalogProvider catalogProvider, Provider<EntityManager> entityManager) {
@@ -20,6 +24,9 @@ public class HibernateCatalogProvider implements Provider<CollectionCatalog> {
 
     @Override
     public CollectionCatalog get() {
-        return catalogProvider.openCatalog(new HibernateQueryExecutor(entityManager));
+        HrdCatalog datastoreCatalog = new HrdCatalog();
+        CollectionCatalog mysqlCatalog = catalogProvider.openCatalog(new HibernateQueryExecutor(entityManager));
+       
+        return new CompositeCatalog(datastoreCatalog, mysqlCatalog);
     }
 }
