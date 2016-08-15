844182910c255ca0850b17c8f9b205b65689f80a
yuriyz <yzabrovarniy@gmail.com>
2016-08-15 11:26:46 +0300

    buildfix

--- core/model/src/main/java/org/activityinfo/model/date/CalendarUtils.java
+++ core/model/src/main/java/org/activityinfo/model/date/CalendarUtils.java
@@ -113,5 +113,47 @@ public class CalendarUtils {
 
         }
         throw new RuntimeException("Failed to identify first day of epic week in year: " + year);
+
     }
+
+    /**
+     * Compares two date with a precision of one second.
+     *
+     * @param baseDate  The base date
+     * @param afterDate The date supposed to be after.
+     * @return True if the afterDate is indeed after the baseDate.
+     */
+    public static boolean after(final Date baseDate, final Date afterDate) {
+        if ((baseDate == null) || (afterDate == null)) {
+            throw new IllegalArgumentException(
+                    "Can't compare the dates, at least one of them is null");
+        }
+
+        final long baseTime = baseDate.getTime() / 1000;
+        final long afterTime = afterDate.getTime() / 1000;
+        return baseTime < afterTime;
+    }
+
+    /**
+     * Compares two date with a precision of one second.
+     *
+     * @param baseDate   The base date
+     * @param beforeDate The date supposed to be before.
+     * @return True if the beforeDate is indeed before the baseDate.
+     */
+    public static boolean before(final Date baseDate, final Date beforeDate) {
+        if ((baseDate == null) || (beforeDate == null)) {
+            throw new IllegalArgumentException(
+                    "Can't compare the dates, at least one of them is null");
+        }
+
+        final long baseTime = baseDate.getTime() / 1000;
+        final long beforeTime = beforeDate.getTime() / 1000;
+        return beforeTime < baseTime;
+    }
+
+    public static LocalDate convert(org.activityinfo.model.type.time.LocalDate date) {
+        return new LocalDate(date.getYear(), date.getMonthOfYear(), date.getDayOfMonth());
+    }
+
 }
