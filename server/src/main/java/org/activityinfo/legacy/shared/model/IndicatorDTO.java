3ff4778f52c9ff77eddeb21307ba08f32f4b5ce3
Alex Bertram <alex@bedatadriven.com>
2016-11-15 16:14:39 +0100

    AI-1463: Copy mandatory flag when cloning databases

--- server/src/main/java/org/activityinfo/legacy/shared/model/IndicatorDTO.java
+++ server/src/main/java/org/activityinfo/legacy/shared/model/IndicatorDTO.java
@@ -367,6 +367,7 @@ public final class IndicatorDTO extends BaseModelData implements EntityDTO, Prov
         field.setLabel(getName());
         field.setDescription(getDescription());
         field.setRelevanceConditionExpression(getRelevanceExpression());
+        field.setRequired(isMandatory());
 
         String code = getNameInExpression();
         if(!Strings.isNullOrEmpty(code)) {
