1896c9c19fbc469f7601a1786c15771774e3c660
Alex Bertram <alex@bedatadriven.com>
2017-03-07 15:30:44 +0100

    Updated unit test

--- core/model/src/test/java/org/activityinfo/model/query/QueryModelTest.java
+++ core/model/src/test/java/org/activityinfo/model/query/QueryModelTest.java
@@ -1,32 +1,9 @@
 package org.activityinfo.model.query;
 
 import org.activityinfo.model.resource.ResourceId;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.io.IOException;
-
-import static org.junit.Assert.assertThat;
-
 public class QueryModelTest {
-
-    @Test
-    public void jacksonTest() throws IOException {
-
-        QueryModel model = new QueryModel(ResourceId.valueOf("XYZ"));
-        model.selectResourceId().as("id");
-        model.selectResourceId();
-        model.selectExpr("foo").as("foo_column");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(model);
-
-        System.out.println(json);
-
-        QueryModel remodel = objectMapper.readValue(json, QueryModel.class);
-        assertThat(remodel.getFilter(), Matchers.nullValue());
-    }
 
     @Test
     public void clientSideSerialization() {
