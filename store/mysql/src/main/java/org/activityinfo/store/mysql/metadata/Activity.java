dc3674a58c9375a93b53f6a8c1841f2c6a710533
Alex Bertram <alex@bedatadriven.com>
2017-05-21 16:54:49 +0200

    Fixed caching of form schemas larger than 65k

--- store/mysql/src/main/java/org/activityinfo/store/mysql/metadata/Activity.java
+++ store/mysql/src/main/java/org/activityinfo/store/mysql/metadata/Activity.java
@@ -1,17 +1,16 @@
 package org.activityinfo.store.mysql.metadata;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
 import com.google.common.collect.*;
+import com.google.gson.JsonParser;
 import org.activityinfo.model.form.FormClass;
 import org.activityinfo.model.legacy.CuidAdapter;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.store.mysql.collections.BETA;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
 public class Activity implements Serializable {
@@ -275,13 +274,22 @@ public class Activity implements Serializable {
             if(value == null) {
                 out.writeBoolean(false);
             } else {
+                // Avoid writeUTF as it has a limit of 65k
                 out.writeBoolean(true);
-                out.writeUTF(value.toJsonString());
+                byte[] bytes = value.toJsonString().getBytes(Charsets.UTF_8);
+                out.writeInt(bytes.length);
+                out.write(bytes);
             }
         }
         private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
             if(in.readBoolean()) {
-                this.value = FormClass.fromJson(in.readUTF());
+                int length = in.readInt();
+                byte[] bytes = new byte[length];
+                in.readFully(bytes);
+                JsonParser parser = new JsonParser();
+                try(InputStreamReader reader = new InputStreamReader(new ByteArrayInputStream(bytes), Charsets.UTF_8)) {
+                    this.value = FormClass.fromJson(parser.parse(reader).getAsJsonObject());
+                }
             } else {
                 this.value = null;
             }
