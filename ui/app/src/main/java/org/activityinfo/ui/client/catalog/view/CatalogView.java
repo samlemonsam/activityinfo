6aa267d6a965fe51201830bb82edf18e5225e920
Alex Bertram <alex@bedatadriven.com>
2017-07-12 06:52:56 +0200

    Wired up "Open Table" button in new catalog view

--- ui/app/src/main/java/org/activityinfo/ui/client/catalog/view/CatalogView.java
+++ ui/app/src/main/java/org/activityinfo/ui/client/catalog/view/CatalogView.java
@@ -1,14 +1,17 @@
 package org.activityinfo.ui.client.catalog.view;
 
 import com.google.common.base.Optional;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.model.form.CatalogEntry;
 import org.activityinfo.model.form.CatalogEntryType;
+import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.observable.Observable;
 import org.activityinfo.ui.client.chrome.HasTitle;
 import org.activityinfo.ui.client.measureDialog.view.CatalogTreeView;
@@ -19,17 +22,19 @@ public class CatalogView implements IsWidget, HasTitle {
     private final VerticalLayoutContainer container;
     private final TextButton openTable;
     private final TextButton newReport;
+    private final CatalogTreeView treeView;
 
     public CatalogView(FormStore formStore, Optional<String> parentId) {
 
         openTable = new TextButton(I18N.CONSTANTS.openTable());
+        openTable.addSelectHandler(this::openTable);
         newReport = new TextButton(I18N.CONSTANTS.createNewReport());
 
         ToolBar bar = new ToolBar();
         bar.add(openTable);
         bar.add(newReport);
 
-        CatalogTreeView treeView = new CatalogTreeView(formStore, parentId, entry -> !entry.getId().equals("geodb"));
+        treeView = new CatalogTreeView(formStore, parentId, entry -> !entry.getId().equals("geodb"));
 
         container = new VerticalLayoutContainer();
         container.add(bar, new VerticalLayoutContainer.VerticalLayoutData(1, -1));
@@ -37,6 +42,14 @@ public class CatalogView implements IsWidget, HasTitle {
 
         treeView.getSelectedEntry().subscribe(this::selectionChanged);
 
+    }
+
+    private void openTable(SelectEvent event) {
+        if(treeView.getSelectedFormId().isLoaded() && treeView.getSelectedFormId().get().isPresent()) {
+            ResourceId formId = treeView.getSelectedFormId().get().get();
+            String url = Window.Location.createUrlBuilder().setHash("table/" + formId.asString()).buildString();
+            Window.open(url, null, null);
+        }
     }
 
     private void selectionChanged(Observable<Optional<CatalogEntry>> selection) {
