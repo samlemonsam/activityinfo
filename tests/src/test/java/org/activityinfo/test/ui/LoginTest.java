5ba9214ca82ab9a3595ac8bf0409eb258a102bf4
Lucy Brinks <lucy@bedatadriven.com>
2017-08-25 14:53:34 +0200

    Added two other tests "space" and "wrong"

--- tests/src/test/java/org/activityinfo/test/ui/LoginTest.java
+++ tests/src/test/java/org/activityinfo/test/ui/LoginTest.java
@@ -2,8 +2,12 @@ package org.activityinfo.test.ui;
 
 import org.activityinfo.test.TestRailCase;
 import org.activityinfo.test.sut.UserAccount;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+import java.net.PasswordAuthentication;
 
 /**
  * Tests that users can log in
@@ -39,5 +43,21 @@ public class LoginTest {
 
         harness.getLoginPage().navigateTo().loginAs(badAccount).assertErrorMessageIsVisible();
     }
+    @Test
+    public void wrong() {
+        UserAccount account = harness.createAccount();
+        UserAccount wrongPassword = new UserAccount(account.getEmail(),"1234");
+
+        harness.getLoginPage().navigateTo().loginAs(wrongPassword).assertErrorMessageIsVisible();
+    }
+
+    @Test
+    public void space() {
+        UserAccount account = harness.createAccount();
+        UserAccount accountSpace = new UserAccount(account.getEmail()+ "   ", account.getPassword());
+
+        harness.getLoginPage().navigateTo().loginAs(accountSpace).andExpectSuccess();
+    }
+
 
 }
