5273001af6eaa78a52c8b18daea9e81f95aa3c20
Alex Bertram <alex@bedatadriven.com>
2017-09-06 10:46:31 +0200

    Updated mailinator client

--- tests/src/test/java/org/activityinfo/test/driver/mail/mailinator/Inbox.java
+++ tests/src/test/java/org/activityinfo/test/driver/mail/mailinator/Inbox.java
@@ -1,9 +1,13 @@
 package org.activityinfo.test.driver.mail.mailinator;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Inbox {
+
     private final List<MessageHeader> headers = new ArrayList<>();
 
     public List<MessageHeader> getMessages() {
