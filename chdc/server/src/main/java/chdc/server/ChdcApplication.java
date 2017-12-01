05b34d483c2d22b0e525642da8c1a6b2ab8abe60
Alex Bertram <alex@bedatadriven.com>
2017-12-01 14:41:44 +0100

    Fixed servlet configuration

--- chdc/server/src/main/java/chdc/server/ChdcApplication.java
+++ chdc/server/src/main/java/chdc/server/ChdcApplication.java
@@ -6,11 +6,18 @@ import org.activityinfo.store.server.JaxRsJsonReader;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * Defines the JAX-RS Application
  */
 public class ChdcApplication extends Application {
+
+    private static final Logger LOGGER = Logger.getLogger(ChdcApplication.class.getName());
+
+    public ChdcApplication() {
+        LOGGER.info("Initializing Chdc Application");
+    }
 
     @Override
     public Set<Object> getSingletons() {
