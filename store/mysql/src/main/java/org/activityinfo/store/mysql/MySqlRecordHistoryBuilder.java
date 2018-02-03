samlemonsam/activityinfo

commit 7e772426b89d5c133c456a25ff49c955af334466

AI-1811 Avoid JsonValue null checks

store/mysql/src/main/java/org/activityinfo/store/mysql/MySqlRecordHistoryBuilder.java
@@ -354,11 +354,11 @@ public class MySqlRecordHistoryBuilder implements RecordHistoryProvider {
                             labelValue = record.get().getFields().get(labelFieldId.get().asString());
                         }
 
-                        if (labelValue == null) {
+                        if (labelValue.isJsonNull()) {
                             labelValue = record.get().getFields().get(CuidAdapter.field(formId, CuidAdapter.NAME_FIELD).asString());
                         }
 
-                        if (labelValue != null && labelValue.isJsonPrimitive()) {
+                        if (labelValue.isJsonPrimitive()) {
                             labelMap.put(ref.getRecordId(), labelValue.asString());
                         }
                     }

model/src/main/java/org/activityinfo/model/form/FormField.java
@@ -268,7 +268,7 @@ public class FormField extends FormElement {
         }
         
         String type;
-        JsonValue typeParameters ;
+        JsonValue typeParameters;
         JsonValue typeElement = jsonObject.get("type");
         
         if(typeElement.isJsonPrimitive()) {
@@ -281,7 +281,7 @@ public class FormField extends FormElement {
         }
         
         FieldTypeClass typeClass = TypeRegistry.get().getTypeClass(type);
-        if(typeClass instanceof ParametrizedFieldTypeClass && typeParameters != null) {
+        if(typeClass instanceof ParametrizedFieldTypeClass && !typeParameters.isJsonNull()) {
             field.setType(((ParametrizedFieldTypeClass) typeClass).deserializeType(typeParameters));
         } else {
             field.setType(typeClass.createType());

model/src/main/java/org/activityinfo/model/query/QueryModel.java
@@ -4,7 +4,6 @@ import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import org.activityinfo.json.Json;
-import org.activityinfo.json.JsonParser;
 import org.activityinfo.json.JsonSerializable;
 import org.activityinfo.json.JsonValue;
 import org.activityinfo.model.expr.ExprNode;
@@ -204,12 +203,9 @@ public class QueryModel implements JsonSerializable {
             queryModel.getColumns().add(ColumnModel.fromJson(column));
         }
 
-        JsonValue filterValue = jsonObject.get("filter");
-        if(filterValue != null && filterValue.isJsonPrimitive()) {
-            String filter = filterValue.asString();
-            if (!Strings.isNullOrEmpty(filter)) {
-                queryModel.setFilter(filter);
-            }
+        String filter = jsonObject.getString("filter");
+        if (!Strings.isNullOrEmpty(filter)) {
+            queryModel.setFilter(filter);
         }
         return queryModel;
     }

model/src/main/java/org/activityinfo/model/type/number/QuantityType.java
@@ -35,7 +35,7 @@ public class QuantityType implements ParametrizedFieldType {
 
             // handling for legacy QuantityType fields (w/ null aggregation) - default to SUM
             String aggregationString = Aggregation.SUM.name();
-            if(!parametersObject.get("aggregation").isJsonNull()) {
+            if(parametersObject.hasKey("aggregation")) {
                 aggregationString = parametersObject.getString("aggregation");
             }
             Aggregation aggregation = Aggregation.valueOf(aggregationString);

store/query/src/main/java/org/activityinfo/store/query/server/Updater.java
@@ -476,7 +476,7 @@ public class Updater {
         Set<String> existingBlobIds = new HashSet<>();
         if(existingResource.isPresent()) {
             JsonValue existingFieldValue = existingResource.get().getFields().get(field.getId().asString());
-            if(existingFieldValue != null) {
+            if(!existingFieldValue.isJsonNull()) {
                 AttachmentValue existingValue = fieldType.parseJsonValue(existingFieldValue);
                 for (Attachment attachment : existingValue.getValues()) {
                     existingBlobIds.add(attachment.getBlobId());

commit 67ab0122fcba847d88fe989e7361a48b5fe030a9

Fixed regression in record history introduced by 7e772426

store/mysql/src/main/java/org/activityinfo/store/mysql/MySqlRecordHistoryBuilder.java
@@ -3,6 +3,7 @@ package org.activityinfo.store.mysql;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Strings;
+import org.activityinfo.json.Json;
 import org.activityinfo.json.JsonValue;
 import org.activityinfo.model.form.*;
 import org.activityinfo.model.legacy.CuidAdapter;
@@ -348,7 +349,7 @@ public class MySqlRecordHistoryBuilder implements RecordHistoryProvider {
                 for (RecordRef ref : value.getReferences()) {
                     Optional<FormRecord> record = form.get().get(ref.getRecordId());
                     if (record.isPresent()) {
-                        JsonValue labelValue = null;
+                        JsonValue labelValue = Json.createNull();
 
                         if (labelFieldId.isPresent()) {
                             labelValue = record.get().getFields().get(labelFieldId.get().asString());