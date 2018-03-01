54ae36a4b954586be914b8b0b4530162c70b4803
Jamie Whitehouse <jamiewhths@gmail.com>
2018-03-01 19:25:09 +0100

    Ensure FormDialog heading uses localized string

--- ui/app/src/main/java/org/activityinfo/ui/client/input/view/FormDialog.java
+++ ui/app/src/main/java/org/activityinfo/ui/client/input/view/FormDialog.java
@@ -3,6 +3,7 @@ package org.activityinfo.ui.client.input.view;
 
 import com.sencha.gxt.widget.core.client.Dialog;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.model.type.RecordRef;
 import org.activityinfo.ui.client.store.FormStore;
@@ -19,7 +20,7 @@ public class FormDialog {
         FormInputView panel = new FormInputView(formStore, recordRef);
 
         dialog = new Dialog();
-        dialog.setHeading("Form");
+        dialog.setHeading(I18N.CONSTANTS.form());
         dialog.setPixelSize(640, 480);
         dialog.setModal(true);
         dialog.setWidget(panel);
