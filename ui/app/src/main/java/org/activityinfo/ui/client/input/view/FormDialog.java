e5eec45b015157302fc8d604e3af15517c7e8288
Jamie Whitehouse <jamiewhths@gmail.com>
2018-03-01 22:06:54 +0100

    AI-1756 Ask if user wants to save form record before closing

--- ui/app/src/main/java/org/activityinfo/ui/client/input/view/FormDialog.java
+++ ui/app/src/main/java/org/activityinfo/ui/client/input/view/FormDialog.java
@@ -2,6 +2,8 @@ package org.activityinfo.ui.client.input.view;
 
 
 import com.sencha.gxt.widget.core.client.Dialog;
+import com.sencha.gxt.widget.core.client.box.ConfirmMessageBox;
+import com.sencha.gxt.widget.core.client.event.DialogHideEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.model.resource.ResourceId;
@@ -11,27 +13,49 @@ import org.activityinfo.ui.client.store.FormStore;
 public class FormDialog {
 
     private ResourceId formId;
-    private FormInputView formInputView;
+    private FormInputView panel;
     private final Dialog dialog;
+    private final ConfirmMessageBox saveWarning;
+    private boolean okPressed = false;
 
     public FormDialog(FormStore formStore, RecordRef recordRef) {
         this.formId = formId;
 
-        FormInputView panel = new FormInputView(formStore, recordRef);
+        panel = new FormInputView(formStore, recordRef);
 
         dialog = new Dialog();
         dialog.setHeading(I18N.CONSTANTS.form());
         dialog.setPixelSize(640, 480);
         dialog.setModal(true);
         dialog.setWidget(panel);
-        dialog.getButton(Dialog.PredefinedButton.OK).addSelectHandler(new SelectEvent.SelectHandler() {
-            @Override
-            public void onSelect(SelectEvent event) {
-                panel.save(closeEvent -> {
-                   dialog.hide();
-                });
-            }
+        dialog.getButton(Dialog.PredefinedButton.OK).addSelectHandler(this::onOk);
+        dialog.addDialogHideHandler(this::onHide);
+
+        saveWarning = new ConfirmMessageBox(I18N.CONSTANTS.warning(), I18N.CONSTANTS.promptSave());
+        saveWarning.setModal(true);
+        saveWarning.getButton(ConfirmMessageBox.PredefinedButton.YES).addSelectHandler(yes -> panel.save(closeHandler -> close()));
+        saveWarning.getButton(ConfirmMessageBox.PredefinedButton.NO).addSelectHandler(no -> close());
+        saveWarning.hide();
+    }
+
+    // User has pressed "x", or has saved the form (i.e. pressed OK)
+    private void onHide(DialogHideEvent hide) {
+        if (!okPressed) {
+            saveWarning.show();
+        }
+    }
+
+    // User has pressed "OK"
+    private void onOk(SelectEvent ok) {
+        panel.save(closeEvent -> {
+            okPressed = true;
+            close();
         });
+    }
+
+    public void close() {
+        saveWarning.hide();
+        dialog.hide();
     }
 
     public void show() {
