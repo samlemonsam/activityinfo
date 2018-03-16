f8e697ae6dea98bd7d48f9677d911e378323cb64
Alex Bertram <alex@bedatadriven.com>
2018-03-16 09:28:28 +0100

    Removed unused methods

--- ui/app2/src/main/java/org/activityinfo/ui/client/util/GwtUtil.java
+++ ui/app2/src/main/java/org/activityinfo/ui/client/util/GwtUtil.java
@@ -18,15 +18,9 @@
  */
 package org.activityinfo.ui.client.util;
 
-import com.google.common.base.Strings;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.NodeList;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
-import org.activityinfo.model.form.FormField;
-
-import java.util.List;
 
 /**
  * @author yuriyz on 1/27/14.
@@ -53,20 +47,6 @@ public class GwtUtil {
         return width < DEFAULT_COLUMN_WIDTH_IN_EM ? DEFAULT_COLUMN_WIDTH_IN_EM : width;
     }
 
-    public static void setVisibleInline(boolean visible, Element... elements) {
-        if (elements != null) {
-            for (Element element : elements) {
-                if (visible) {
-                    element.removeClassName("hidden");
-                    element.addClassName("show-inline");
-                } else {
-                    element.removeClassName("show-inline");
-                    element.addClassName("hidden");
-                }
-            }
-        }
-    }
-
     public static void setVisible(boolean visible, Element... elements) {
         if (elements != null) {
             for (Element element : elements) {
@@ -79,10 +59,6 @@ public class GwtUtil {
                 }
             }
         }
-    }
-
-    public static String valueWithTooltip(String value) {
-        return "<span qtip='" + value + "'>" + value + "</span>";
     }
 
     public static ScrollPanel getScrollAncestor(Widget widget) {
@@ -114,57 +90,8 @@ public class GwtUtil {
         }
     }
 
-    public static boolean isInViewport(com.google.gwt.user.client.Element element) {
-        return GwtUtil.getBsContainerRectangle(element).has(GwtUtil.getRectangle(element));
-    }
-
     public static Rectangle getRectangle(Element element) {
         return new Rectangle(element.getAbsoluteLeft(), element.getAbsoluteRight(), element.getAbsoluteTop(), element.getAbsoluteBottom());
     }
 
-    public static Rectangle getBsContainerRectangle(com.google.gwt.user.client.Element descendant) {
-        return getRectangle(getBsContainer(descendant));
-    }
-
-    public static com.google.gwt.user.client.Element getBsContainer(com.google.gwt.user.client.Element descendant) {
-        if (descendant != null) {
-            if (descendant.getClassName().contains("bs ")) {
-                return descendant;
-            } else if (descendant.getParentElement() != null) {
-                return getBsContainer(DOM.asOld(descendant.getParentElement()));
-            }
-            return descendant;
-        }
-        return null;
-    }
-
-    public static void addTooltipToOptions(Element containerElement, int maxCharacters, List<FormField> fields) {
-        NodeList<Element> elements = containerElement.getElementsByTagName("option");
-        for (int i = 0; i < elements.getLength(); i++) {
-            Element item = elements.getItem(i);
-
-            String innerText = item.getInnerText();
-            item.setTitle(innerText);
-
-            if (innerText.length() > maxCharacters) {
-                innerText = innerText.substring(0, maxCharacters) + "...";
-
-                String code = getCodeById(fields, item.getAttribute("value"));
-                if (!Strings.isNullOrEmpty(code)) {
-                    innerText = code + " - "+ innerText;
-                }
-
-                item.setInnerText(innerText);
-            }
-        }
-    }
-
-    private static String getCodeById(List<FormField> fields, String value) {
-        for (FormField field : fields) {
-            if (field.getId().asString().equals(value)) {
-                return field.getCode();
-            }
-        }
-        return "";
-    }
 }
