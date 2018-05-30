75121a218df878fb8ee142d155f262afa9aa2bdd
Alex Bertram <alex@bedatadriven.com>
2018-05-30 22:35:12 +0200

    Fix integration test

--- server/src/test/java/org/activityinfo/server/login/ConfirmInviteControllerTest.java
+++ server/src/test/java/org/activityinfo/server/login/ConfirmInviteControllerTest.java
@@ -19,6 +19,8 @@
 package org.activityinfo.server.login;
 
 import com.google.inject.util.Providers;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.util.Closeable;
 import com.sun.jersey.api.view.Viewable;
 import org.activityinfo.server.authentication.AuthTokenProvider;
 import org.activityinfo.server.database.hibernate.dao.AuthenticationDAO;
@@ -29,6 +31,7 @@ import org.activityinfo.server.login.model.ConfirmInvitePageModel;
 import org.activityinfo.server.login.model.InvalidInvitePageModel;
 import org.activityinfo.server.util.MailingListClient;
 import org.easymock.EasyMock;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -51,16 +54,16 @@ public class ConfirmInviteControllerTest {
     private ConfirmInviteController resource;
     private User user;
 
+    private Closeable objectify;
+
     @Before
     public final void setup() {
 
         user = new User();
 
         userDAO = createMock(UserDAO.class);
-        expect(userDAO.findUserByChangePasswordKey(eq(VALID_KEY))).andReturn(
-                user);
-        expect(userDAO.findUserByChangePasswordKey(EasyMock.not(eq(VALID_KEY))))
-                .andThrow(new NoResultException());
+        expect(userDAO.findUserByChangePasswordKey(eq(VALID_KEY))).andReturn(user);
+        expect(userDAO.findUserByChangePasswordKey(EasyMock.not(eq(VALID_KEY)))).andThrow(new NoResultException());
         replay(userDAO);
 
         AuthenticationDAO authDAO = createMock(AuthenticationDAO.class);
@@ -75,6 +78,13 @@ public class ConfirmInviteControllerTest {
 
         resource = new ConfirmInviteController(
                 Providers.of(userDAO), authTokenProvider, mailingListClient);
+
+        objectify = ObjectifyService.begin();
+    }
+
+    @After
+    public void tearDown() {
+        objectify.close();
     }
 
     @Test
