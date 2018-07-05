4fa799e82eae1d61602abac8394f1fe0d854861b
Alex Bertram <alex@bedatadriven.com>
2018-07-05 17:01:44 +0200

    Starting full integration tests for column block queries

---
+++ store/hrd/src/test/java/org/activityinfo/store/hrd/HrdFormStorageTest.java
@@ -0,0 +1,80 @@
+/*
+ * ActivityInfo
+ * Copyright (C) 2009-2013 UNICEF
+ * Copyright (C) 2014-2018 BeDataDriven Groep B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.activityinfo.store.hrd;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.util.Closeable;
+import net.lightoze.gwt.i18n.server.LocaleProxy;
+import org.activityinfo.store.spi.TypedRecordUpdate;
+import org.activityinfo.store.testing.RecordGenerator;
+import org.activityinfo.store.testing.Survey;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HrdFormStorageTest {
+
+    private static final int USER_ID = 1;
+    private final LocalServiceTestHelper helper =
+            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig()
+                    .setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+    
+    private int userId = 1;
+    private Closeable objectifyCloseable;
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+        objectifyCloseable = ObjectifyService.begin();
+    }
+    
+    @BeforeClass
+    public static void setUpLocale() {
+        LocaleProxy.initialize();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+        objectifyCloseable.close();
+    }
+
+    @Test
+    public void surveyTest() {
+
+        Survey surveyForm = new Survey();
+
+        HrdStorageProvider storageProvider = new HrdStorageProvider();
+        HrdFormStorage storage = storageProvider.create(surveyForm.getFormClass());
+
+        RecordGenerator generator = surveyForm.getGenerator();
+
+        int numRecords = 100_342;
+
+        for (int i = 0; i < numRecords; i++) {
+            storage.add(new TypedRecordUpdate(USER_ID, generator.get()));
+        }
+
+
+
+    }
+}
---
+++ store/hrd/src/test/java/org/activityinfo/store/hrd/columns/SingleEnumBlockTest.java
@@ -0,0 +1,5 @@
+package org.activityinfo.store.hrd.columns;
+
+public class SingleEnumBlockTest {
+
+}
