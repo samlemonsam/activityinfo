d21e682b706c37a86653971ae7d4340b8a641dd4
Jamie Whitehouse <jamiewhths@gmail.com>
2018-07-13 16:02:04 +0200

    AI-1941 Decouple PostmarkWebhookTest from CommandTestCase

--- server/src/test/java/org/activityinfo/server/mail/PostmarkWebhookTest.java
+++ server/src/test/java/org/activityinfo/server/mail/PostmarkWebhookTest.java
@@ -1,44 +1,58 @@
 package org.activityinfo.server.mail;
 
 import com.google.inject.Inject;
+import com.google.inject.util.Providers;
 import org.activityinfo.fixtures.InjectionSupport;
 import org.activityinfo.fixtures.Modules;
-import org.activityinfo.server.command.CommandTestCase;
+import org.activityinfo.fixtures.TestHibernateModule;
+import org.activityinfo.server.DeploymentConfiguration;
 import org.activityinfo.server.database.OnDataSet;
 import org.activityinfo.server.database.hibernate.entity.User;
+import org.activityinfo.server.util.config.ConfigModuleStub;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(InjectionSupport.class)
-@Modules(MailSenderStubModule.class)
+@Modules({
+        TestHibernateModule.class,
+        ConfigModuleStub.class
+})
 @OnDataSet("/dbunit/clone-database.db.xml")
-public class PostmarkWebhookTest extends CommandTestCase {
+public class PostmarkWebhookTest {
 
     @Inject
-    private PostmarkWebhook postmarkWebhook;
+    protected EntityManager em;
 
-    private BounceReport bounceReport;
+    @Inject
+    protected DeploymentConfiguration config;
+
+    private PostmarkWebhook postmarkWebhook;
 
     @Before
     public void setUp() {
-        this.bounceReport = new BounceReport();
-        bounceReport.setRecordType("Bounce");
-        bounceReport.setEmail("akbertram@gmail.com");
+        postmarkWebhook = new PostmarkWebhook(config, Providers.of(em));
     }
 
     @Test
     public void bounceEmail() {
         // Get user with email notifications enabled
         User user = em.find(User.class, 1);
-        assert(user.isEmailNotification());
+        assertTrue(user.isEmailNotification());
 
         // Bounce email
+        BounceReport bounceReport = new BounceReport();
+        bounceReport.setRecordType("Bounce");
+        bounceReport.setEmail("akbertram@gmail.com");
         postmarkWebhook.bounce("test_token", bounceReport);
 
         // Ensure user now has email notifications disabled
         user = em.find(User.class, 1);
-        assert(!user.isEmailNotification());
+        assertTrue(!user.isEmailNotification());
     }
 
 }
