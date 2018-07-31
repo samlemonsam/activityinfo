3c0b56c4fcd7677bc56f9a54fa791462343e5a8c
Jamie Whitehouse <jamiewhths@gmail.com>
2018-07-31 13:30:33 +0200

    AI-1714 Fix extraction of monthly and subform records

--- server/src/main/java/org/activityinfo/server/job/ExportLongFormatExecutor.java
+++ server/src/main/java/org/activityinfo/server/job/ExportLongFormatExecutor.java
@@ -1,16 +1,22 @@
 package org.activityinfo.server.job;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import org.activityinfo.analysis.pivot.LongFormatTableBuilder;
 import org.activityinfo.legacy.shared.command.GetSchema;
 import org.activityinfo.legacy.shared.model.ActivityDTO;
+import org.activityinfo.legacy.shared.model.ActivityFormDTO;
 import org.activityinfo.legacy.shared.model.UserDatabaseDTO;
 import org.activityinfo.model.analysis.pivot.*;
 import org.activityinfo.model.form.FormClass;
+import org.activityinfo.model.form.FormField;
 import org.activityinfo.model.job.ExportLongFormatJob;
 import org.activityinfo.model.job.ExportPivotTableJob;
 import org.activityinfo.model.job.ExportResult;
+import org.activityinfo.model.legacy.CuidAdapter;
+import org.activityinfo.model.resource.ResourceId;
+import org.activityinfo.model.type.subform.SubFormReferenceType;
 import org.activityinfo.server.command.DispatcherSync;
 import org.activityinfo.server.generated.StorageProvider;
 import org.activityinfo.store.query.shared.FormSource;
@@ -18,6 +24,7 @@ import org.activityinfo.store.spi.FormStorageProvider;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ExportLongFormatExecutor implements JobExecutor<ExportLongFormatJob, ExportResult> {
@@ -27,6 +34,21 @@ public class ExportLongFormatExecutor implements JobExecutor<ExportLongFormatJob
     private final DispatcherSync dispatcher;
     private final FormSource formSource;
 
+    private static Predicate<ActivityDTO> activity() {
+        return activity -> activity.getClassicView() && (activity.getReportingFrequency() == ActivityFormDTO.REPORT_ONCE);
+    }
+
+    private static Predicate<ActivityDTO> monthlyActivity() {
+        return activity -> activity.getClassicView() && (activity.getReportingFrequency() == ActivityFormDTO.REPORT_MONTHLY);
+    }
+
+    private static Predicate<ActivityDTO> form() {
+        return activity -> !activity.getClassicView();
+    }
+
+    private static Predicate<FormField> subFormField() {
+        return field -> field.getType() instanceof SubFormReferenceType;
+    }
 
     @Inject
     public ExportLongFormatExecutor(FormStorageProvider formStorageProvider,
@@ -56,7 +78,16 @@ public class ExportLongFormatExecutor implements JobExecutor<ExportLongFormatJob
     }
 
     private List<FormClass> getFormScope(UserDatabaseDTO database) {
+        List<FormClass> scope = Lists.newArrayList();
+        scope.addAll(getActivityForms(database));
+        scope.addAll(getMonthlyActivityForms(database));
+        scope.addAll(getForms(database));
+        return scope;
+    }
+
+    private List<FormClass> getActivityForms(UserDatabaseDTO database) {
         return database.getActivities().stream()
+                .filter(activity())
                 .map(ActivityDTO::getFormId)
                 .map(formStorageProvider::getForm)
                 .filter(Optional::isPresent)
@@ -64,5 +95,47 @@ public class ExportLongFormatExecutor implements JobExecutor<ExportLongFormatJob
                 .collect(Collectors.toList());
     }
 
+    private List<FormClass> getMonthlyActivityForms(UserDatabaseDTO database) {
+        return database.getActivities().stream()
+                .filter(monthlyActivity())
+                .map(ActivityDTO::getId)
+                .map(CuidAdapter::reportingPeriodFormClass)
+                .map(formStorageProvider::getForm)
+                .filter(Optional::isPresent)
+                .map(presentForm -> presentForm.get().getFormClass())
+                .collect(Collectors.toList());
+    }
 
+    private List<FormClass> getForms(UserDatabaseDTO database) {
+        List<FormClass> scope = Lists.newArrayList();
+        scope.addAll(getParentForms(database));
+        scope.addAll(getSubForms(scope));
+        return scope;
+    }
+
+    private List<FormClass> getParentForms(UserDatabaseDTO database) {
+        return database.getActivities().stream()
+                .filter(form())
+                .map(ActivityDTO::getFormId)
+                .map(formStorageProvider::getForm)
+                .filter(Optional::isPresent)
+                .map(presentForm -> presentForm.get().getFormClass())
+                .collect(Collectors.toList());
+    }
+
+    private List<FormClass> getSubForms(List<FormClass> parentForms) {
+        return parentForms.stream()
+                .flatMap(form -> form.getFields().stream())
+                .filter(subFormField())
+                .map(ExportLongFormatExecutor::subFormId)
+                .map(formStorageProvider::getForm)
+                .filter(Optional::isPresent)
+                .map(presentForm -> presentForm.get().getFormClass())
+                .collect(Collectors.toList());
+    }
+
+    private static ResourceId subFormId(FormField refField) {
+        SubFormReferenceType type = (SubFormReferenceType) refField.getType();
+        return type.getClassId();
+    }
 }
