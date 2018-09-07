80428b41090450b5787c26f7ccfb3270740e242f
Alex Bertram <alex@bedatadriven.com>
2018-09-07 21:17:45 +0200

    Fixed unit test

--- server/src/test/java/org/activityinfo/server/login/HostViewTest.java
+++ server/src/test/java/org/activityinfo/server/login/HostViewTest.java
@@ -39,6 +39,7 @@ public class HostViewTest extends ViewTestCase {
 
         HostPageModel pageModel = new HostPageModel(
                 "http://www.activityinfo.org");
+        pageModel.setLocale("en");
         pageModel.setBootstrapScript("/ActivityInfo/ActivityInfo.nocache.js");
         assertProcessable(pageModel);
     }
