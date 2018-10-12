a4de8afbedf3b4bcdc457a5129ca3b7082755b8b
Jamie Whitehouse <jamiewhths@gmail.com>
2018-10-12 16:17:07 +0200

    AI-1986 Add getter for a specific resource on UserDatabaseMeta

--- model/src/main/java/org/activityinfo/model/database/UserDatabaseMeta.java
+++ model/src/main/java/org/activityinfo/model/database/UserDatabaseMeta.java
@@ -89,6 +89,9 @@ public class UserDatabaseMeta {
         return new RecordLockSet(effective);
     }
 
+    public Resource getResource(ResourceId resourceId) {
+        return resources.get(resourceId);
+    }
 
     public String getVersion() {
         return version;
