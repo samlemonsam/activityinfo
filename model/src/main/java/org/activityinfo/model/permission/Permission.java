ff02e6a0174767eb8986386c47d0079764f5c97b
Jamie Whitehouse <jamiewhths@gmail.com>
2018-11-08 11:26:46 +0100

    Remove hasFilter() assert from getFilter(); return nullable filter

--- model/src/main/java/org/activityinfo/model/permission/Permission.java
+++ model/src/main/java/org/activityinfo/model/permission/Permission.java
@@ -5,6 +5,8 @@ import org.activityinfo.json.Json;
 import org.activityinfo.json.JsonSerializable;
 import org.activityinfo.json.JsonValue;
 
+import javax.annotation.Nullable;
+
 /**
  * <p>Response from a {@link PermissionQuery}. Specifies whether the operation requested is permitted.</p>
  * <p>If the operation <i>is</i> permitted, the (optional) filter is to be applied to any queries for this operation.</p>
@@ -44,9 +46,8 @@ public class Permission implements JsonSerializable {
         this.permitted = permitted;
     }
 
-    public String getFilter() {
-        assert filter.isPresent() : "No filter defined for current Permission.";
-        return filter.get();
+    public @Nullable String getFilter() {
+        return filter.orNull();
     }
 
     public void setFilter(Optional<String> filter) {
