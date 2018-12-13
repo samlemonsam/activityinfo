740d6b1cb3cc69b6b8b8a7072b98f7a22f3f9069
Jamie Whitehouse <jamiewhths@gmail.com>
2018-12-13 16:29:01 +0100

    Add DatabaseProviderTest on dataset

---
+++ server/src/test/java/org/activityinfo/server/endpoint/rest/DatabaseProviderTest.java
@@ -0,0 +1,182 @@
+package org.activityinfo.server.endpoint.rest;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.inject.Inject;
+import org.activityinfo.fixtures.InjectionSupport;
+import org.activityinfo.fixtures.Modules;
+import org.activityinfo.fixtures.TestHibernateModule;
+import org.activityinfo.model.database.UserDatabaseMeta;
+import org.activityinfo.model.legacy.CuidAdapter;
+import org.activityinfo.server.database.DatabaseModule;
+import org.activityinfo.server.database.OnDataSet;
+import org.activityinfo.store.spi.DatabaseProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+@RunWith(InjectionSupport.class)
+@Modules({
+        TestHibernateModule.class,
+        DatabaseModule.class
+})
+@OnDataSet("/dbunit/schema4.db.xml")
+public class DatabaseProviderTest {
+
+    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+    private static final int IRAQ_DB = 1;
+    private static final int SYRIA_DB = 2;
+    private static final int LEBANON_DB = 3;
+    private static final int DELETED_DB = 4;
+    private static final int NON_EXISTENT_DB = 999;
+
+    private static final int ALEX = 1;
+    private static final int BAVON = 2;
+    private static final int JOHN = 3;
+    private static final int JACOB = 4;
+
+    @Inject
+    private DatabaseProvider databaseProvider;
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        helper.tearDown();
+    }
+
+    @Test
+    public void missingDatabase() {
+        Optional<UserDatabaseMeta> db = databaseProvider.getDatabaseMetadata(NON_EXISTENT_DB, ALEX);
+        // Missing database should not be present
+        assertFalse(db.isPresent());
+    }
+
+    @Test
+    public void deletedDatabase() {
+        Optional<UserDatabaseMeta> db = databaseProvider.getDatabaseMetadata(DELETED_DB, ALEX);
+        // Deleted database should be present but marked as deleted with no data
+        assertTrue(db.isPresent());
+        assertTrue(db.get().isDeleted());
+        assertTrue(db.get().getGrants().isEmpty());
+        assertTrue(db.get().getResources().isEmpty());
+        assertTrue(db.get().getLocks().isEmpty());
+        assertThat(db.get().getLabel(), equalTo(""));
+        assertThat(db.get().getDescription(), equalTo(""));
+    }
+
+    @Test
+    public void database_alex() {
+        Optional<UserDatabaseMeta> iraqDb = databaseProvider.getDatabaseMetadata(IRAQ_DB, ALEX);
+        Optional<UserDatabaseMeta> syriaDb = databaseProvider.getDatabaseMetadata(SYRIA_DB, ALEX);
+
+        // IRAQ: Should have a returned UserDatabaseMeta which is entirely visible. Owner has no grants.
+        // Database has 7 resources in total
+        assertTrue(iraqDb.isPresent());
+        assertTrue(iraqDb.get().isVisible());
+        assertTrue(iraqDb.get().isOwner());
+        assertThat(iraqDb.get().getResources().size(), equalTo(7));
+        assertTrue(iraqDb.get().getGrants().isEmpty());
+
+        // SYRIA: Should have a returned UserDatabaseMeta which is entirely visible. Owner has no grants.
+        // Database has 7 resources in total
+        assertTrue(syriaDb.isPresent());
+        assertTrue(syriaDb.get().isVisible());
+        assertTrue(syriaDb.get().isOwner());
+        assertThat(syriaDb.get().getResources().size(), equalTo(7));
+        assertTrue(syriaDb.get().getGrants().isEmpty());
+    }
+
+    @Test
+    public void database_bavon() {
+        Optional<UserDatabaseMeta> iraqDb = databaseProvider.getDatabaseMetadata(IRAQ_DB, BAVON);
+        Optional<UserDatabaseMeta> syriaDb = databaseProvider.getDatabaseMetadata(SYRIA_DB, BAVON);
+        Optional<UserDatabaseMeta> lebanonDb = databaseProvider.getDatabaseMetadata(LEBANON_DB, BAVON);
+
+        // IRAQ: Should have a returned UserDatabaseMeta which is invisible as user has no grants and no public resources
+        assertTrue(iraqDb.isPresent());
+        assertFalse(iraqDb.get().isVisible());
+        assertTrue(iraqDb.get().getResources().isEmpty());
+        assertTrue(iraqDb.get().getGrants().isEmpty());
+
+        // SYRIA: Should have a returned UserDatabaseMeta which is invisible as user has no grants and no public resources
+        assertTrue(syriaDb.isPresent());
+        assertFalse(syriaDb.get().isVisible());
+        assertTrue(syriaDb.get().getResources().isEmpty());
+        assertTrue(syriaDb.get().getGrants().isEmpty());
+
+        // LEBANON: Should have a returned UserDatabaseMeta which is entirely visible. Owner has no grants.
+        // Database has 7 resources in total
+        assertTrue(lebanonDb.isPresent());
+        assertTrue(lebanonDb.get().isVisible());
+        assertTrue(lebanonDb.get().isOwner());
+        assertThat(lebanonDb.get().getResources().size(), equalTo(7));
+        assertTrue(lebanonDb.get().getGrants().isEmpty());
+    }
+
+    @Test
+    public void database_john() {
+        Optional<UserDatabaseMeta> iraqDb = databaseProvider.getDatabaseMetadata(IRAQ_DB, JOHN);
+        Optional<UserDatabaseMeta> syriaDb = databaseProvider.getDatabaseMetadata(SYRIA_DB, JOHN);
+        Optional<UserDatabaseMeta> lebanonDb = databaseProvider.getDatabaseMetadata(LEBANON_DB, JOHN);
+
+        // IRAQ: Should have a returned UserDatabaseMeta which is invisible as user has no grants and no public resources
+        assertTrue(iraqDb.isPresent());
+        assertFalse(iraqDb.get().isVisible());
+        assertTrue(iraqDb.get().getResources().isEmpty());
+        assertTrue(iraqDb.get().getGrants().isEmpty());
+
+        // SYRIA: Should have a returned UserDatabaseMeta which is visible. User has grant on Folder 4, with associated resources visible (3).
+        assertTrue(syriaDb.isPresent());
+        assertTrue(syriaDb.get().isVisible());
+        assertThat(syriaDb.get().getGrants().size(), equalTo(1));
+        assertTrue(syriaDb.get().hasGrant(CuidAdapter.folderId(4)));
+        assertThat(syriaDb.get().getResources().size(), equalTo(3));
+
+        // LEBANON: Should have a returned UserDatabaseMeta which is visible. User has grant on Folder 7, with associated resources visible (3).
+        // Public INTAKE FORM and SUBFORM resources are also visible, bringing total visible resources to 5.
+        assertTrue(lebanonDb.isPresent());
+        assertTrue(lebanonDb.get().isVisible());
+        assertThat(lebanonDb.get().getGrants().size(), equalTo(1));
+        assertTrue(lebanonDb.get().hasGrant(CuidAdapter.folderId(7)));
+        assertThat(lebanonDb.get().getResources().size(), equalTo(5));
+    }
+
+    @Test
+    public void database_jacob() {
+        Optional<UserDatabaseMeta> iraqDb = databaseProvider.getDatabaseMetadata(IRAQ_DB, JACOB);
+        Optional<UserDatabaseMeta> syriaDb = databaseProvider.getDatabaseMetadata(SYRIA_DB, JACOB);
+        Optional<UserDatabaseMeta> lebanonDb = databaseProvider.getDatabaseMetadata(LEBANON_DB, JACOB);
+
+        // IRAQ: Should have a returned UserDatabaseMeta which is invisible as user has no grants and no public resources
+        assertTrue(iraqDb.isPresent());
+        assertFalse(iraqDb.get().isVisible());
+        assertTrue(iraqDb.get().getResources().isEmpty());
+        assertTrue(iraqDb.get().getGrants().isEmpty());
+
+        // SYRIA: Should have a returned UserDatabaseMeta which is invisible as user has no grants and no public resources
+        assertTrue(syriaDb.isPresent());
+        assertFalse(syriaDb.get().isVisible());
+        assertTrue(syriaDb.get().getResources().isEmpty());
+        assertTrue(syriaDb.get().getGrants().isEmpty());
+
+        // LEBANON: Should have a returned UserDatabaseMeta which is visible as there are public resources
+        // Should be 2 resources visible: Intake Form and its Sub Form
+        assertTrue(lebanonDb.isPresent());
+        assertTrue(lebanonDb.get().isVisible());
+        assertThat(lebanonDb.get().getResources().size(), equalTo(2));
+        assertTrue(lebanonDb.get().getGrants().isEmpty());
+    }
+
+}
