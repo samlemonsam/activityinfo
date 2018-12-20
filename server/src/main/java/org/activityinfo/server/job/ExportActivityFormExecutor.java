b5ff4e2c2fab1b13163fbbd93120176f8c5e1313
Jamie Whitehouse <jamiewhths@gmail.com>
2018-12-20 10:13:48 +0100

    Inject required resources into ExportActivityFormExecutor

--- server/src/main/java/org/activityinfo/server/job/ExportActivityFormExecutor.java
+++ server/src/main/java/org/activityinfo/server/job/ExportActivityFormExecutor.java
@@ -25,6 +25,7 @@ import org.activityinfo.server.command.handler.AttributeFilterMap;
 import org.activityinfo.server.command.handler.QueryFilterBuilder;
 import org.activityinfo.server.generated.StorageProvider;
 import org.activityinfo.store.query.shared.FormSource;
+import org.activityinfo.store.spi.DatabaseProvider;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -38,23 +39,26 @@ public class ExportActivityFormExecutor implements JobExecutor<ExportActivityFor
 
     private FormSource formSource;
     private StorageProvider storageProvider;
+    private DatabaseProvider databaseProvider;
     private Provider<DispatcherSync> dispatcher;
     private Provider<AuthenticatedUser> authUser;
 
     @Inject
     public ExportActivityFormExecutor(FormSource formSource,
                                       StorageProvider storageProvider,
+                                      DatabaseProvider databaseProvider,
                                       Provider<DispatcherSync> dispatcher,
                                       Provider<AuthenticatedUser> authUser) {
         this.formSource = formSource;
         this.storageProvider = storageProvider;
+        this.databaseProvider = databaseProvider;
         this.dispatcher = dispatcher;
         this.authUser = authUser;
     }
 
     @Override
     public ExportResult execute(ExportActivityFormJob descriptor) throws IOException {
-        ExportFormExecutor formExporter = new ExportFormExecutor(formSource, storageProvider);
+        ExportFormExecutor formExporter = new ExportFormExecutor(formSource, storageProvider, databaseProvider, authUser.get());
 
         Filter filter = FilterUrlSerializer.fromUrlFragment(descriptor.getFilter());
         ResourceId activityFormId = fetchFormId(filter);
