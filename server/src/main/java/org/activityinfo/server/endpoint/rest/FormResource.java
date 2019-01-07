eb696480cfcc98d471f79b4d9dbd631fd267ea92
Alex Bertram <alex@bedatadriven.com>
2019-01-07 14:33:51 +0100

    AI-2016 Add /query/rows/full endpoint

--- server/src/main/java/org/activityinfo/server/endpoint/rest/FormResource.java
+++ server/src/main/java/org/activityinfo/server/endpoint/rest/FormResource.java
@@ -400,12 +400,25 @@ public class FormResource {
     @Path("query/rows")
     @Produces(MediaType.APPLICATION_JSON)
     public Response queryRows(@Context UriInfo uriInfo) {
+        return queryRows(uriInfo, false);
+    }
+
+    @GET
+    @NoCache
+    @Path("query/rows/full")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response queryFullRows(@Context UriInfo uriInfo) {
+        return queryRows(uriInfo, true);
+    }
+
+    private Response queryRows(@Context UriInfo uriInfo, boolean includeEmptyFields) {
         final ColumnSet columnSet = query(uriInfo);
 
         LOGGER.info("Query completed with " + columnSet.getNumRows() + " rows.");
 
         final StreamingOutput output = outputStream -> {
             RowBasedJsonWriter writer = new RowBasedJsonWriter(outputStream, Charsets.UTF_8);
+            writer.setIncludeEmpyFields(includeEmptyFields);
             writer.write(columnSet);
             writer.flush();
         };
@@ -415,6 +428,7 @@ public class FormResource {
             .type(JSON_CONTENT_TYPE)
             .build();
     }
+
 
     @GET
     @NoCache
