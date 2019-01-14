3c602e7bfa9b3bf18fa23298fc50fa341063ed56
Jamie Whitehouse <jamiewhths@gmail.com>
2019-01-14 12:37:27 +0100

    Increment DatabaseMeta cache version

--- server/src/main/java/org/activityinfo/server/database/hibernate/HibernateDatabaseMetaProvider.java
+++ server/src/main/java/org/activityinfo/server/database/hibernate/HibernateDatabaseMetaProvider.java
@@ -38,7 +38,7 @@ public class HibernateDatabaseMetaProvider implements DatabaseMetaProvider {
     private final Logger LOGGER = Logger.getLogger(HibernateDatabaseGrantProvider.class.getName());
 
     private static final String CACHE_PREFIX = "dbMeta";
-    private static final String CACHE_VERSION = "1";
+    private static final String CACHE_VERSION = "2";
 
     private final Provider<EntityManager> entityManager;
     private final FormStorageProvider formStorageProvider;
